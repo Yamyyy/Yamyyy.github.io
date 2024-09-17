7f94e497818942978a831b99dfe12f2be8e1f00d
Yamyyy <2396649622@qq.com>
2024-09-17 13:42:08 +0800

    auto update

--- auto_update.py
+++ auto_update.py
@@ -11,23 +11,27 @@ response = requests.request("GET", url, headers=headers, data=payload)
 # print(response.text)
 
 new_soup = BeautifulSoup(response.text)
+print(new_soup)
 if new_soup.find('ul', class_='slot-list') is None:
     print("302")
 else:
-    old_soup = BeautifulSoup(open("./gymbooker.html"))
+    with open("健身房预约首页 _ 翔安校区体育馆预约系统.html", "r", encoding='utf-8') as f:
+        html = f.read()
+        old_soup = BeautifulSoup(html, 'html.parser')
 
-    old_soup.find('ul', class_='slot-list').clear()
-
-
-    # empty_div = BeautifulSoup('<div class="clearfix"></div>')
-
-    for i in new_soup.find('ul', class_='slot-list'):
-        old_soup.find('ul', class_='slot-list').append(i)
-        old_soup.find('ul', class_='slot-list').append(BeautifulSoup('<div class="clearfix"></div>'))
-
-    old_soup.find('ul', class_='slot-list').append(new_soup.find('div', class_="help-info"))
+        old_soup.find('ul', class_='slot-list').clear()
 
 
-    with open('gymbooker.html', 'w', encoding='utf-8') as f:
+        # empty_div = BeautifulSoup('<div class="clearfix"></div>')
+
+        for i in new_soup.find('ul', class_='slot-list'):
+            old_soup.find('ul', class_='slot-list').append(i)
+            old_soup.find('ul', class_='slot-list').append(BeautifulSoup('<div class="clearfix"></div>'))
+
+        old_soup.find('ul', class_='slot-list').append(new_soup.find('div', class_="help-info"))
+        print(old_soup)
+
+
+    with open('健身房预约首页 _ 翔安校区体育馆预约系统.html', 'w', encoding='utf-8') as f:
         f.write(str(old_soup))  # write result
     print("successed")
---
+++ git_gymbooker_scheduler.py
@@ -0,0 +1,38 @@
+import os
+import time
+
+def GetTime(): 
+    return time.strftime("%y-%m-%d %H:%M:%S", time.localtime())
+
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+from datetime import datetime, date
+ 
+
+ 
+ 
+def execute():
+    print("start execute")
+    print(GetTime())
+    
+    commit_msg="Code updated:"+GetTime() 
+
+    cmd_1 = "git status"
+    cmd_2 = "git add ."
+    cmd_3 = "git commit -m"+'/"'+commit_msg+'/"'
+    cmd_4 = "git push"
+
+    os.system("python /data/mxp/junjie/Yamyyy.github.io/auto_update.py")
+    # os.system(cmd_1)
+    os.system(cmd_2)
+    os.system(cmd_3)
+    os.system(cmd_4)
+
+    print("succesed")
+
+
+execute()
+scheduler = BlockingScheduler()
+
+scheduler.add_job(execute,'interval',seconds=7200)
+scheduler.start()
